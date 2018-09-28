6847d8f5e164b453a00d0a77405fa7562e93df89
hyeongnam.jang <hyeongnam.jang@lge.com>
2018-09-28 17:17:07 +0900

    complete palindrome.c

--- c_lang/list_ex/palindrome.c
+++ c_lang/list_ex/palindrome.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct node {
 	struct node * next;
@@ -32,10 +33,35 @@ void list_del_entry(struct node * e) {
 	list_del_gen(e->prev, e->next);
 }
 
+struct node * reverse_list(struct node * head) {
+	struct node * src = head->prev;
+	struct node * res = malloc(sizeof(res));
+	struct node * c;
+	init_list(res);
+	for (; src!=head; src = src->prev) {
+		//printf("%s: (%c) src:%p src->prev:%p\n",__func__, src->data, src, src->prev);
+		c = malloc(sizeof(c));
+		c->data = src->data;
+		list_add_tail(c, res);
+	}
+	return res;
+}
+
+bool compare_list(struct node * a, struct node * b) {
+	struct node * x = a->next;
+	struct node * y = b->next;
+	for(; (x!=a); (x = x->next, y = y->next)) {
+		if (y==b) return false;
+		if (x->data != y->data) return false;
+	}
+	if (y!=b) return false;
+	return true;
+}
+
 struct node * str2list(char * str) {
 	struct node * head = malloc(sizeof(head));
 	struct node * c;
-	printf("%s: entry %s(%c)\n",__func__,str, *str);
+	//printf("%s: entry %s(%c)\n",__func__,str, *str);
 	init_list(head);
 	for (; *str!='\0'; str++) {
 		//printf("%s: (%c)\n",__func__, *str);
@@ -51,9 +77,9 @@ void print_list(struct node * head) {
 	struct node * cur = head->next;
 	for (; cur!=head; cur = cur->next) {
 		printf("%d[%c:%X]\t", cur->data, cur->data, cur->data);
+		printf("ptr:%p/%p/%p\t", cur->prev, cur, cur->next);
 	}
 }
-
 
 int main(int argc, const char *argv[]) {
 	char * tc[6] = {
@@ -61,11 +87,16 @@ int main(int argc, const char *argv[]) {
 	};
 	int i;
 	struct node * head[6];
+	struct node * rev[6];
 	for (i=0; i<6; i++) {
 		head[i] = str2list(tc[i]);
-		printf("i=%d head[i]=%p\n", i, head[i]);
-		print_list(head[i]);
-		printf("i=%d done.\n", i);
+		//print_list(head[i]);
+		rev[i] = reverse_list(head[i]);
+		printf("i=%d %s\n", i, tc[i]);
+		if (compare_list(rev[i],head[i]))
+			printf("Palindrome\n");
+		else
+			printf("Not a palindrome\n");
 	}
 
 	return 0;
