c4eb92e755e24e367e739f63a28def7e77bbf6c6
Hyeongnam Jang <hnjang810@gmail.com>
2018-12-02 14:46:44 +0900

    update youtube-audio

--- py/youtube-audio/youtube-audio.py
+++ py/youtube-audio/youtube-audio.py
@@ -3,13 +3,19 @@ import sys
 import os
 import glob
 
-rl = lambda: sys.stdin.readline().strip()
-I = lambda: int(rl())
 
-n = I()
+def read_int(string=None):
+    return int(input(string))
+
+output_type = input()
+if output_type not in ['def', 'mp3']:
+    print('output_type is %s. something worng' % output_type)
+    sys.exit(-1)
 l = []
-for i in range(n):
-    l.append(rl())
+while True:
+    inp = input()
+    if inp=='end': break
+    l.append(inp)
 
 TMP_DIR = './tmp'
 
@@ -19,14 +25,19 @@ except:
     pass
 os.chdir(TMP_DIR)
 for ll in l:
+    if len(ll)==0: break
     os.system('youtube-dl -f 140 %s'%(ll))
     g = glob.glob('*.m4a')
     if len(g)<=0: continue
-    os.system('ffmpeg -i \"%s\" \"%s.mp3\"'%(g[0],g[0]))
-    print('renaming...')
-    dest = os.path.join('..','%s.mp3'%g[0])
-    os.rename('%s.mp3'%g[0], dest)
-    os.remove(g[0])
+    if output_type != 'mp3':
+        src = g[0]
+    else:
+        os.system('ffmpeg -i \"%s\" \"%s.mp3\"'%(g[0],g[0]))
+        dest = os.path.join('..','%s.mp3'%g[0])
+        os.remove(g[0])
+        src = '%s.mp3'%g[0]
+    dest = os.path.join('..', src)
+    os.rename(src, dest)
 
 os.chdir('..')
 os.rmdir(TMP_DIR)
