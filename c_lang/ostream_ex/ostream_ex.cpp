517a1b711db011654d770026c06ceb863a00d741
Hyeongnam Jang <hnjang810@gmail.com>
2018-12-16 18:22:48 +0900

    update ostream_ex

--- c_lang/ostream_ex/ostream_ex.cpp
+++ c_lang/ostream_ex/ostream_ex.cpp
@@ -1,18 +1,26 @@
 #include <vector>
+#include <array>
 #include <string>
 #include <cstdint>
 #include <iostream>
 
-class my_length {
+class Length {
+struct Units {
+	int limit;
+	double divisor;
+	std::string name;
+};
+	static const std::array<Units,4> units;
 	int id;
 	std::string name;
 	uint64_t v;		// unit is 'mm'
 	bool fmt_auto;
 
 	public:
-	my_length (int id, std::string name, uint64_t v)
+	Length (int id, std::string name, uint64_t v)
 		: id{id}, name{name}, v{v}, fmt_auto(false) {}
-	friend std::ostream &operator<<(std::ostream &out, const my_length &l) {
+#if 0
+	friend std::ostream &operator<<(std::ostream &out, const Length &l) {
 		out << l.id << "/" << l.name << " : ";
 		if (!l.fmt_auto) {
 			return out << l.v << "mm";
@@ -27,20 +35,47 @@ class my_length {
 				return out << double(l.v)/1000000 << "km";
 		}
 	}
+#endif
+	const Units& selectUnit() const {
+		// if fmt_auto, select units automatically.
+		// otherwise, use default unit
+		auto ret = units.begin();
+		if (fmt_auto) {
+			while (ret->limit && v >= ret->limit) {
+				++ret;
+				}
+		}
+		return *ret;
+	}
+	friend std::ostream &operator<<(std::ostream &out, const Length &l) {
+		const Units u = l.selectUnit();
+		return out << l.id << "" << l.name << " : "
+			<< l.v/u.divisor << u.name;
+	}
+
 	void set_fmt_auto(bool b) {fmt_auto = b;}
 };
+
+const std::array<Length::Units,4> Length::units { {
+{ 10, 1, "mm"},
+{ 1000, 10, "cm"},
+{ 1000000, 1000, "m"},
+{ 0, 1000000, "km"}
+} };
+
 
 bool need_fmt_auto(int x) {
 	return (x%2) != 0;
 }
 
 int main() {
-	std::vector<my_length> lengths;
-	my_length l{0, "a", 1};
+	std::string test("a");
+	std::vector<Length> lengths;
+	Length l{0, std::string("a"), 1};
 	lengths.push_back(l);
 	uint64_t val = 17;
 	for (int i=1; i<10; i++) {
-		my_length l{i, std::string(1, 'a'+i), val};
+		Length l{i, std::string(1, 'a'+i), val};
 		lengths.push_back(l);
 		val *= 10;
 	}
