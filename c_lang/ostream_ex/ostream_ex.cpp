4d9bdd59e9527e454dc4b75fd4d84f245f2da6d8
Hyeongnam Jang <hnjang810@gmail.com>
2018-12-17 11:01:33 +0900

    update ostream_ex and add clang-format file

--- c_lang/ostream_ex/ostream_ex.cpp
+++ c_lang/ostream_ex/ostream_ex.cpp
@@ -5,67 +5,49 @@
 #include <iostream>
 
 class Length {
-struct Units {
-	int limit;
-	double divisor;
-	std::string name;
-};
-	static const std::array<Units,4> units;
+	struct Units {
+		int limit;
+		double divisor;
+		std::string name;
+	};
+	static const std::array<Units, 4> units;
 	int id;
 	std::string name;
-	uint64_t v;		// unit is 'mm'
+	uint64_t v; // unit is 'mm'
 	bool fmt_auto;
 
-	public:
-	Length (int id, std::string name, uint64_t v)
+  public:
+	Length(int id, std::string name, uint64_t v)
 		: id{id}, name{name}, v{v}, fmt_auto(false) {}
-#if 0
-	friend std::ostream &operator<<(std::ostream &out, const Length &l) {
-		out << l.id << "/" << l.name << " : ";
-		if (!l.fmt_auto) {
-			return out << l.v << "mm";
-		} else {
-			if (l.v<10)
-				return out << l.v << "mm";
-			else if (l.v<1000)
-				return out << double(l.v)/10 << "cm";
-			else if (l.v<1000000)
-				return out << double(l.v)/1000 << "m";
-			else
-				return out << double(l.v)/1000000 << "km";
-		}
-	}
-#endif
-	const Units& selectUnit() const {
+	const Units &selectUnit() const {
 		// if fmt_auto, select units automatically.
 		// otherwise, use default unit
 		auto ret = units.begin();
 		if (fmt_auto) {
 			while (ret->limit && v >= ret->limit) {
 				++ret;
-				}
+			}
 		}
 		return *ret;
 	}
 	friend std::ostream &operator<<(std::ostream &out, const Length &l) {
 		const Units u = l.selectUnit();
-		return out << l.id << "" << l.name << " : "
-			<< l.v/u.divisor << u.name;
+		return out << l.id << "" << l.name << " : " << l.v / u.divisor
+				   << u.name;
 	}
 
-	void set_fmt_auto(bool b) {fmt_auto = b;}
+	void set_fmt_auto(bool b) { fmt_auto = b; }
 };
 
-const std::array<Length::Units,4> Length::units { {
-{ 10, 1, "mm"},
-{ 1000, 10, "cm"},
-{ 1000000, 1000, "m"},
-{ 0, 1000000, "km"}
-} };
-
+const std::array<Length::Units, 4> Length::units {{
+	{10, 1, "mm"},
+	{1000, 10, "cm"},
+	{1000000, 1000, "m"},
+	{0, 1000000, "km"}
+}};
 
 bool need_fmt_auto(int x) {
-	return (x%2) != 0;
+	return (x % 2) != 0;
 }
 
 int main() {
@@ -74,12 +56,12 @@ int main() {
 	Length l{0, std::string("a"), 1};
 	lengths.push_back(l);
 	uint64_t val = 17;
-	for (int i=1; i<10; i++) {
-		Length l{i, std::string(1, 'a'+i), val};
+	for (int i = 1; i < 10; i++) {
+		Length l{i, std::string(1, 'a' + i), val};
 		lengths.push_back(l);
 		val *= 10;
 	}
-	for (int i=0; i<lengths.size(); i++) {
+	for (int i = 0; i < lengths.size(); i++) {
 		if (need_fmt_auto(i)) {
 			lengths[i].set_fmt_auto(true);
 			std::cout << "fmt_auto: ";
