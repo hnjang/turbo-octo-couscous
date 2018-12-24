130b452c80b5be891b2d13d65bc26c612669ca4f
Hyeongnam Jang <hnjang810@gmail.com>
2018-12-24 16:42:24 +0900

    HR: upload solutions for NLP
     - These solution used NLTK, but HR does not support NLTK.

---
+++ hr/ai/paragraph_sentence/paragraph_sentence.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+import sys
+import time
+from pprint import pprint
+import nltk
+from nltk.tokenize import sent_tokenize
+
+
+def is_simple_quote(string):
+    if string[0]=='\"' and string[-1]=='\"': return True
+    else: return False
+
+def tokenize_postwork(l):
+    out = []
+    while len(l)>2:
+        if is_simple_quote(l[0]) and not is_simple_quote(l[1]):
+            out.append(' '.join(l[0:2]))
+            l = l[2:]
+        else:
+            out.append(l[0])
+            l = l[1:]
+    if len(l)>0:
+        out.extend(l)
+    return out
+
+
+def tokenize_prework(text):
+    out = []
+    words = text.split()
+    for w in words:
+        # put whitespace after ? or !
+        for ec in ['?', '!']:
+            if ec in w[:-1]:
+                w = w.replace(ec, ec + ' ')
+        # put whitespace after '.'
+        if '.' in w[:-1]:
+            out2 = []
+            s = w[:-1].split('.')
+            while len(s) > 2:
+                if s[0] in ['Dr', 'Mr', 'Ms', 'Mrs']:
+                    out2.appned('%s.%s' % (s[0], s[1]))
+                    s = s[2:]
+            if len(s) > 0:
+                out2.extend(s)
+            out2[-1] = out2[-1] + w[-1]
+            out.append('. '.join(out2))
+            # print('L36')
+            # pprint(out[-1])
+            continue
+        # there is no middle-punctuation
+        out.append(w)
+        '''
+        print('=== start dump')
+        pprint(out)
+        '''
+    return ' '.join(out)
+
+
+if __name__ == '__main__':
+    nltk.download('punkt')
+    text = input()
+    text = tokenize_prework(text)
+    '''
+    print('=== result of pretokenize_adjust')
+    pprint(text)
+    print('=== end of dump')
+    '''
+    tokenized = sent_tokenize(text)
+    result = tokenize_postwork(tokenized)
+    for s in result:
+        print(s)
+
