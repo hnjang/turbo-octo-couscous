30b0fe87a93a268d9aa6e72b787db179256c0c89
Hyeongnam Jang <hnjang810@gmail.com>
2018-12-30 23:36:40 +0900

    update py tutorial source

--- py_tutorial/first_step/hello_world/hello_world.py
+++ py_tutorial/first_step/hello_world/hello_world.py
@@ -1,2 +1,2 @@
 name = input('당신의 이름은 무엇인가요? ')
-print('{}님, 안녕하세요.'.format(name))
+print(name, '님, 안녕하세요.')
--- py_tutorial/first_step/password_checker/password_checker.py
+++ py_tutorial/first_step/password_checker/password_checker.py
@@ -1,10 +1,10 @@
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 password = input('비밀번호를 입력하세요. ')
-print('비밀번호의 길이는 {}입니다.'.format(len(password)))
+print('비밀번호의 길이는', len(password), '입니다.')
 found = []
 for letter in password:
     if letter in numbers:
         found.append(letter)
-print('숫자를 {}개 사용하셨습니다.'.format(len(found)))
+print('숫자를', len(found), '개 사용하셨습니다.')
 for f in found:
     print(f)
