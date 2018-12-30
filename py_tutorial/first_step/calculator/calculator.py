9c01f0850e9c30c775d4b5694e759ab0bc05ee63
Hyeongnam Jang <hnjang810@gmail.com>
2018-12-31 01:06:36 +0900

    update calculator.py

--- py_tutorial/first_step/calculator/calculator.py
+++ py_tutorial/first_step/calculator/calculator.py
@@ -1,7 +1,7 @@
 
 def tokenize(string):
     result = []
-    op_str = '+-*/()'
+    op_paren_str = '+-*/()'
     p = 0
     while True:
         if len(string) == 0:
@@ -9,21 +9,46 @@ def tokenize(string):
         if len(string) <= p:
             result.append(string)
             break
-        if string[p] not in op_str:
+        if string[p] not in op_paren_str:
             p += 1
         else:
-            result.append(string[:p])
+            if p > 0:
+                result.append(string[:p])
             result.append(string[p])
             string = string[p+1:]
             p = 0
     return result
 
 def make_postfix(infix):
-    s = 0
-    op_str = '+-*/()'
-    p = 0
+    op_str = '+-*/'
+    op_rank = {
+        '+' : 1, '-' : 1,
+        '*' : 2, '/' : 2,
+		'(' : 3
+        }
+    stack = []
+    postfix = []
+    for e in infix:
+        if e == '(':
+            stack.append(e)
+        elif e == ')':
+            op = stack.pop()
+            while op != '(':
+                postfix.append(op)
+                op = stack.pop()
+        elif e in op_str:
+            while len(stack) > 0 and op_rank[e] >= op_rank[stack[-1]]:
+                postfix.append(stack.pop())
+            stack.append(e)
+        else:
+            postfix.append(e)
+
+    while len(stack) > 0:
+        postfix.append(stack.pop())
+    return postfix
 
 expr = input()
 infix = tokenize(expr)
 print(infix)
-
+postfix = make_postfix(infix)
+print(postfix)
