ee08b83200ee3cb30305076b32832900b0107d2d
Hyeongnam Jang <hnjang810@gmail.com>
2019-03-22 22:30:02 +0900

    upload gdb_ex and python_gdb_script.py

---
+++ c_lang/gdb_ex/gdb_ex.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct ApplicationState {
+    struct {
+        bool use_crash_handler;
+        bool use_abort_handler;
+    } signal;
+
+    struct {
+        unsigned char python;
+    } exit_code_on_error;
+};
+
+int main() {
+	struct ApplicationState as;
+	as.signal.use_crash_handler = true;
+	as.signal.use_abort_handler = false;
+	as.exit_code_on_error.python = 'X';
+	abort();
+}
