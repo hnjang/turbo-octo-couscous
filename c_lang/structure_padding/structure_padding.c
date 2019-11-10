0573fc0e7a8f4c6947e36dda4f2bf10d49aaad73
hnjang810 <hnjang810@gmail.com>
2019-11-10 15:28:35 +0900

    add new .clang-format style file and apply it

--- c_lang/structure_padding/structure_padding.c
+++ c_lang/structure_padding/structure_padding.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 // Alignment requirements
 // (typical 32 bit machine)
@@ -10,47 +10,53 @@
 // double       8 bytes
 
 // structure A
-typedef struct structa_tag {
-    char c;
-    //short int s;
-    int16_t s;
+typedef struct structa_tag
+{
+   char c;
+   // short int s;
+   int16_t s;
 } structa_t;
 
-struct a_packed {
-    char c;
-    //short int s;
-    int16_t s;
-}__attribute__((packed));
+struct a_packed
+{
+   char c;
+   // short int s;
+   int16_t s;
+} __attribute__((packed));
 
 // structure B
-typedef struct structb_tag {
-    //short int s;
-    int16_t s;
-    char c;
-    int i;
+typedef struct structb_tag
+{
+   // short int s;
+   int16_t s;
+   char c;
+   int i;
 } structb_t;
 
 // structure C
-typedef struct structc_tag {
-    char c;
-    double d;
-    int s;
+typedef struct structc_tag
+{
+   char c;
+   double d;
+   int s;
 } structc_t;
 
 // structure D
-typedef struct structd_tag {
-    double d;
-    int s;
-    char c;
+typedef struct structd_tag
+{
+   double d;
+   int s;
+   char c;
 } structd_t;
 
-int main() {
-    printf("sizeof(int) = %lu\n", sizeof(int));
-    printf("sizeof(structa_t) = %lu\n", sizeof(structa_t));
-    printf("sizeof(structa_t) = %lu\n", sizeof(struct a_packed));
-    printf("sizeof(structb_t) = %lu\n", sizeof(structb_t));
-    printf("sizeof(structc_t) = %lu\n", sizeof(structc_t));
-    printf("sizeof(structd_t) = %lu\n", sizeof(structd_t));
+int main()
+{
+   printf("sizeof(int) = %lu\n", sizeof(int));
+   printf("sizeof(structa_t) = %lu\n", sizeof(structa_t));
+   printf("sizeof(structa_t) = %lu\n", sizeof(struct a_packed));
+   printf("sizeof(structb_t) = %lu\n", sizeof(structb_t));
+   printf("sizeof(structc_t) = %lu\n", sizeof(structc_t));
+   printf("sizeof(structd_t) = %lu\n", sizeof(structd_t));
 
-    return 0;
+   return 0;
 }
