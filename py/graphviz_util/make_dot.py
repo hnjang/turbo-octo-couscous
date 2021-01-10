d3659c0c18948ae4acc3163a06f3d8cf5a9e45e3
hnjang810 <hnjang810@gmail.com>
2021-01-10 15:17:18 +0900

    update graphviz util -- 2nd update for today

--- py/graphviz_util/make_dot.py
+++ py/graphviz_util/make_dot.py
@@ -15,21 +15,51 @@ with open(in_fname, 'r') as in_f:
     
 decoded = json.loads(data)
 contents = decoded['contents']
-links = [__ for __ in contents if __['type']=='link']
-comments = [__ for __ in contents if __['type']=='comment']
+# set default value for 'group'
+for cc in contents:
+    #print(cc)
+    #print(cc.get('group',''))
+    cc['group'] = cc.get('group','')
+
+links = [__ for __ in contents if __['group'] != 'comment']
+comments = [__ for __ in contents if __['group']=='comment']
 
 #pprint(contents)
 
 out_fname = in_fname +'.dot'
 with open(out_fname, 'w') as out_f:
+    # Put head contents
+    out_f.write('digraph {\n')
+    
     # Put links
     for ll in links:
-        out_f.write('  %s -> %s\n'%(ll['nodes'][0], ll['nodes'][1]))
-    out_f.write('\n#now, here is comments\n\n')
+        if ll['type']=='link':
+            out_f.write('  %s -> %s\n'%(ll['nodes'][0], ll['nodes'][1]))
+        elif ll['type']=='switch':
+            for i, ch in enumerate(ll['nodes'][1:]):
+                out_f.write('  %s -> %s[label="%s"]\n'%(ll['nodes'][0], ch, ll['labels'][i]))
+        elif ll['type']=='end-switch':
+            for i, parent in enumerate(ll['nodes'][:-1]):
+                out_f.write('  %s -> %s\n'%(parent, ll['nodes'][-1]))
+        else:
+            print('unrecognized type: %s.'%ll['type'])
+            print('dump the element...')
+            pprint(ll)
+                
+    out_f.write('\n#now, here are comments\n\n')
     for cc in comments:
         #cmt_node_name = unicode.join(u'', [cc['node'],'_cmt'])
-        cmt_node_name = cc['node'] + '_cmt'
-        out_f.write('  %s -> %s[arrowhead="none"]\n'%(cc['node'], cmt_node_name))
-        out_f.write('  %s[label="%s"]\n'%(cmt_node_name, cc['comment']))
+        try:
+            cmt_node_name = cc['node'] + '_cmt'
+            out_f.write('  %s -> %s[arrowhead="none"]\n'%(cc['node'], cmt_node_name))
+            out_f.write('  %s[label="%s"]\n'%(cmt_node_name, cc['comment']))
+        except:
+            print('exception! check if the element has values for "node", "comment".')
+            print('dump the element...')
+            pprint(cc)
+
+    # Put tail contents
+    out_f.write('}\n')
+
     
 print('output file is created: %s'%out_fname)
--- py/graphviz_util/make_output.py
+++ py/graphviz_util/make_output.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 
 import sys
+import os
 import glob
 
-target_path = '.\\'
+conf = dict()
+conf['dot_bin'] = r'D:\download\graphviz-2.44.1-win32\Graphviz\bin\dot.exe'
+
 g = glob.glob('.\\*.dot')
-gout_fname = ['%s\\%s.svg' % (target_path, gg) for gg in g]
-magick_out_fname = ['%s\\%s.png' % (target_path, gg) for gg in g]
+gout_fname = ['.\\%s.svg' % (gg) for gg in g]
+magick_out_fname = ['.\\%s.png' % (gg) for gg in g]
 
 for i, gg in enumerate(g):
-    os.system('%s -Tsvg %s > %s' % (dot_bin, gg, gout_fname[i]))
+    os.system('%s -Tsvg %s > %s' % (conf['dot_bin'], gg, gout_fname[i]))
 print('SVG files are created')
 
 for i , infile in enumerate(gout_fname):
     if not os.path.exists(magick_out_fname[i]):
-        os.system('magick convert -density 600 %s %s'%infile, magick_out_fname[i])
+        os.system('magick convert -density 600 %s %s'%(infile, magick_out_fname[i]))
     else:
-        print('SKip converting(outfile exists: %s)'%magick_out_fname[i])
+        print('Skip converting(outfile exists: %s)'%magick_out_fname[i])
